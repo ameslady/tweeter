733eb1012f376ffc327716cb07eda71b5d0d229c
Amy Martin <amymariemart@gmail.com>
2022-04-26 20:46:28 -0700

    Update compose-char-counter to use addClass jquery

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,19 +1,19 @@
 
 $(document).ready(function() {
   const textarea = $("#tweet-text");
-  const total = Number($(".counter").html());
+  const total = $(".counter").html();
 
   textarea.on('keyup', function() {
     const valueLength = $(this).val().length;
     const output = $(this).parent().find(".counter");
     const remaining = total - valueLength;
 
-    output.html(String(remaining))
+    output.html(remaining)
 
     if (remaining < 0) {
-      output.css("color", "red")
+      output.addClass("red");
     } else {
-      output.css("color", "#2F2B34")
+      output.removeClass("red");
     }
   });
 });
