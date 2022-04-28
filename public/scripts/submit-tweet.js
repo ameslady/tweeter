5f9c6f16bb3f0dd93cb0ad4504b3d9f420a5ef45
Amy Martin <amymariemart@gmail.com>
2022-04-27 21:21:18 -0700

    Implement validation on tweet POST

--- public/scripts/submit-tweet.js
+++ public/scripts/submit-tweet.js
@@ -3,10 +3,18 @@ $(document).ready(function() {
 
   $( "#form" ).submit(function(event) {
     event.preventDefault();
-    // update to $.ajax later
-    $.post("/tweets", $(this).serialize());
-    $('#tweet-text').val('');
-    $('.counter').val(140);
+
+    const input = $('#tweet-text').val();
+
+    if (input === '') {
+      alert("How dare thee submit a blank tweet!")
+    } else if (input.length > 140) {
+      alert("Thy tweet is too long!")
+    } else {
+      $.post("/tweets", $(this).serialize());
+      $('#tweet-text').val('');
+      $('.counter').val(140);
+    }
   });
 
 });
